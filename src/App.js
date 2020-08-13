2431f54b3444f1dea105544dc29126bab5ee5f15
Akolshorn <AndreasKol@users.noreply.github.com>
2020-08-13 00:58:50 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';      // Person should be in upper case. Lower case reserved with lower case. ..C
+
+class App extends Component 
+{
+   // app = props => {
+     state = {
+      persons: [
+        {name: 'Max', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Steph', age: 246}
+      ],
+      otherState: 'Some other value'
+    };
+   // ;
+
+switchNameHandler = () => {       //using es6 function vs es5 where you need a this.
+   // DONT" DO this:  this.state.person[0].name = 'Maxmilian';
+   // Rather do this:   Merges below with state. SetState and props two things that change DOM
+  this.setState( { persons: [
+    {name: 'Maximil', age: 28},
+    {name: 'Manu', age: 29},
+    {name: 'Steph', age: 24}
+  ]
+});
+ 
+  // console.log('Was Clicked')
+};
+
+// Do not use () below on onClick or it calls as a function. We want a reference
+// In regular JS its onclick vs here onClick
+
+render() {
+   return (
+      <div className="App">
+       <h1>Hi, I'm a react app</h1>
+       <p>This is really working.!</p>
+
+       <button onClick={this.switchNameHandler}>Switch Name</button>    
+                                                                        
+       <Person name= {this.state.persons[0].name}  age={this.state.persons[0].age} />
+       <Person name= {this.state.persons[1].name}  age={this.state.persons[1].age} />
+       <Person name= {this.state.persons[2].name}  age={this.state.persons[2].age} />Likes: Racing<Person/>
+      </div>
+    ); 
+    //return React.createElement ('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'))
+  }
+
+};
+
+
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props) => {    // convention use props
+    
+    return (
+    <div>
+        <p>I'm {props.name} Person I'm  {props.age}  {Math.floor(Math.random()*30)} years old!</p>
+        <p>{props.children}</p>  
+    </div> 
+    )
+}
+
+export default person
